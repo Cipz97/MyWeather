08afef9fbc211a90a8e7e76265bef7a58525dc49
Cipz <matteo.cipone@gmail.com>
2021-10-15 17:00:46 +0200

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Matteo Cipone on 15.10.21.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
